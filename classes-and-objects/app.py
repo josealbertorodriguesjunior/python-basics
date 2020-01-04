95de06f664f60e60cc06d1abec517eeb1d29b6b5
Junior Lima <josealbertorodriguesjunior@gmail.com>
2020-01-03 21:22:01 -0300

    initial commit

---
+++ classes-and-objects/app.py
@@ -0,0 +1,25 @@
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+car1 = Vehicle()
+car2 = Vehicle()
+#car1 sets
+car1.color = "Red"
+car1.kind = "Convertible"
+car1.value = 60000.00
+car1.name = "Fer"
+#car2 sets
+car2.color = "Blue"
+car2.kind = "Van"
+car2.value = 10000.00
+car2.name = "Jump"
+# test code
+print(car1.description())
+print(car2.description())
